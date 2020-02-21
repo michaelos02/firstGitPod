e6c868bb6b38daf7435431536725b8bba63d0fe8
michaelos02 <mro.michael@gmail.com>
2020-02-21 00:38:20 +0000

    added web page

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-function runTet(){
-    Logger.log("Hello, world");
+function runTest(){
+    Logger.log("Hello, there you handsome devil!");
+    let ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1");
+    let data = ss.getRange("A1").getValue();
+    Logger.log(data);
 }
