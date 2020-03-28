23859fc7de6886bccc34f4458f787c375dd46c2c
michaelos02 <mro.michael@gmail.com>
2020-03-28 18:54:40 +0000

    Bid JS, CSS and code update

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
+function onOpen(){
+  var ui = SpreadsheetApp.getUi(); 
+  ui.createMenu("Show dialog")
+    .addItem("Show it", 'serveIt')
+    .addToUi();
+}
 function serveIt(){
-    return HtmlService.createTemplateFromFile('start').evaluate;
+    let html = HtmlService.createTemplateFromFile('start').evaluate();
+    SpreadsheetApp.getUi().showSidebar(html);
+}
 
+
+//get css and js into the web page
+function include(filename){
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
